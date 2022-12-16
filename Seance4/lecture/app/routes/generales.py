72c9266856120fc3ab2fa78807bf0f4cddb427b0
Maxime Challon <maxime.challon@gmail.com>
2022-12-16 12:46:08 +0100

    [Seance4] lecture -- get

--- Seance4/lecture/app/routes/generales.py
+++ Seance4/lecture/app/routes/generales.py
@@ -32,3 +32,18 @@ def first():
         })
     
     return render_template("pages/all.html", donnees=donnees, sous_titre="First")
+
+@app.route("/get")
+def get():
+    donnees = []
+
+    query =  Country.query
+    resultat = query.get('fr')
+
+    donnees.append({
+            "nom": resultat.name,
+            "description": resultat.Introduction,
+            "type": resultat.type
+        })
+    
+    return render_template("pages/all.html", donnees=donnees, sous_titre="Get")
