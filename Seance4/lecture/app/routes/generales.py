d9db84a5612e8e9967bf952c7d2cd3e1d5d8b790
Maxime Challon <maxime.challon@gmail.com>
2022-12-16 13:21:32 +0100

    [Seance4] lecture -- and et or

--- Seance4/lecture/app/routes/generales.py
+++ Seance4/lecture/app/routes/generales.py
@@ -1,6 +1,7 @@
 from ..app import app, db
 from flask import render_template
 from ..models.factbook import Country
+from sqlalchemy import and_, or_
 
 @app.route("/all")
 def all():
@@ -47,3 +48,83 @@ def get():
         })
     
     return render_template("pages/all.html", donnees=donnees, sous_titre="Get")
+
+@app.route("/filter")
+def filter():
+    donnees = []
+
+    query =  Country.query
+    resultats = query.filter(Country.type == 'sovereign').all()
+
+    for resultat in resultats:
+        donnees.append({
+            "nom": resultat.name,
+            "description": resultat.Introduction,
+            "type": resultat.type
+        })
+    
+    return render_template("pages/all.html", donnees=donnees, sous_titre="Filter")
+
+@app.route("/filter_and_implicite")
+def filter_and_implicite():
+    donnees = []
+
+    query =  Country.query
+    resultats = query.filter(Country.type == 'sovereign', Country.id == 'fr' ).all()
+
+    for resultat in resultats:
+        donnees.append({
+            "nom": resultat.name,
+            "description": resultat.Introduction,
+            "type": resultat.type
+        })
+    
+    return render_template("pages/all.html", donnees=donnees, sous_titre="And implicite")
+
+@app.route("/filter_and_explicite")
+def filter_and_explicite():
+    donnees = []
+
+    query =  Country.query
+    resultats = query.filter(and_(Country.type == 'sovereign', Country.id == 'fr' )).all()
+
+    for resultat in resultats:
+        donnees.append({
+            "nom": resultat.name,
+            "description": resultat.Introduction,
+            "type": resultat.type
+        })
+    
+    return render_template("pages/all.html", donnees=donnees, sous_titre="And explicite")
+
+@app.route("/filter_or_implicite")
+def filter_or_implicite():
+    donnees = []
+
+    query =  Country.query
+    resultats = query.filter((Country.type == 'dependency')|(Country.id == 'fr')).all()
+
+    for resultat in resultats:
+        donnees.append({
+            "nom": resultat.name,
+            "description": resultat.Introduction,
+            "type": resultat.type
+        })
+    
+    return render_template("pages/all.html", donnees=donnees, sous_titre="Or implicite")
+
+@app.route("/filter_or_explicite")
+def filter_or_explicite():
+    donnees = []
+
+    query =  Country.query
+    resultats = query.filter(or_(Country.type == 'dependency', Country.id == 'fr' )).all()
+
+    for resultat in resultats:
+        donnees.append({
+            "nom": resultat.name,
+            "description": resultat.Introduction,
+            "type": resultat.type
+        })
+    
+    return render_template("pages/all.html", donnees=donnees, sous_titre="Or explicite")
