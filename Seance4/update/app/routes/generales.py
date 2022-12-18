840d589729ba6188f14782c2b140675f023201a9
Maxime Challon <maxime.challon@gmail.com>
2022-12-18 10:40:18 +0100

    [Seance4] update -- correction route

--- Seance4/update/app/routes/generales.py
+++ Seance4/update/app/routes/generales.py
@@ -3,7 +3,7 @@ from flask import render_template
 from ..models.factbook import  Resources
 
 @app.route("/update_ressource/<string:current_name>/<string:new_name>")
-def add_ressource(current_name, new_name):
+def update_ressource(current_name, new_name):
     
     Resources.query.filter(Resources.name == current_name).update({"name": new_name})
     db.session.commit()
