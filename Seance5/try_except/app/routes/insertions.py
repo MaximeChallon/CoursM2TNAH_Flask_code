64cfce2d28ba81deab73b8d904844e958c610a2a
Maxime Challon <maxime.challon@gmail.com>
2023-01-29 14:33:50 +0100

    [Seance5] try_except

--- Seance5/try_except/app/routes/insertions.py
+++ Seance5/try_except/app/routes/insertions.py
@@ -8,30 +8,34 @@ from ..utils.transformations import  clean_arg
 def insertion_pays():
     form = InsertionPays() 
 
-    if form.validate_on_submit():
-        nom_pays =  clean_arg(request.form.get("nom_pays", None))
-        code_pays =  clean_arg(request.form.get("code_pays", None))
-        type =  clean_arg(request.form.get("type", None))
-        introduction =  clean_arg(request.form.get("introduction", None))
-        ressources =  clean_arg(request.form.getlist("ressources", None))
-        continent =  clean_arg(request.form.get("continent", None))
+    try:
+        if form.validate_on_submit():
+            nom_pays =  clean_arg(request.form.get("nom_pays", None))
+            code_pays =  clean_arg(request.form.get("code_pays", None))
+            type =  clean_arg(request.form.get("type", None))
+            introduction =  clean_arg(request.form.get("introduction", None))
+            ressources =  clean_arg(request.form.getlist("ressources", None))
+            continent =  clean_arg(request.form.get("continent", None))
 
-        nouveau_pays = Country(id=code_pays, 
-            Introduction=introduction,
-            name=nom_pays,
-            type = type)
+            nouveau_pays = Country(id=code_pays, 
+                Introduction=introduction,
+                name=nom_pays,
+                type = type)
 
-        for ressource in ressources:
-            ressource = Resources.\
-                query.\
-                filter(Resources.id == ressource).\
-                first()
-            nouveau_pays.resources.append(ressource)
-        
-        nouveau_pays.maps.append(Map.query.filter(Map.name==continent).first())
+            for ressource in ressources:
+                ressource = Resources.\
+                    query.\
+                    filter(Resources.id == ressource).\
+                    first()
+                nouveau_pays.resources.append(ressource)
+            
+            nouveau_pays.maps.append(Map.query.filter(Map.name==continent).first())
 
-        db.session.add(nouveau_pays)
-        db.session.commit()
+            db.session.add(nouveau_pays)
+            db.session.commit()
+    
+    except Exception as e :
+        print(e)
     
     return render_template("pages/insertion_pays.html", 
             sous_titre= "Insertion pays" , 
