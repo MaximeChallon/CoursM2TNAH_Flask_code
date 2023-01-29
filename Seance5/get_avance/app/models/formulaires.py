df57871643337782a31d3679225ffd28ba8c1bd4
Maxime Challon <maxime.challon@gmail.com>
2023-01-29 13:58:05 +0100

    [Seance5] get_avance maj coquille

--- Seance5/get_avance/app/models/formulaires.py
+++ Seance5/get_avance/app/models/formulaires.py
@@ -4,4 +4,4 @@ from wtforms import StringField, SelectField
 class Recherche(FlaskForm):
     nom_pays = StringField("nom_pays", validators=[]) 
     ressources = SelectField('ressources', choices=[('', ''),('PET', 'pétrole'), ('GOL', 'or')])
-    continents = SelectField('ressources', choices=[('', ''),('Europe', 'Europe'), ('Asia', 'Asie'), ('Africa', 'Afrique')])
+    continents = SelectField('continents', choices=[('', ''),('Europe', 'Europe'), ('Asia', 'Asie'), ('Africa', 'Afrique')])
